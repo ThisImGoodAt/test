6215e8532bc3d06bb11387d49c06545422d22dad
rileyverrall <rileyverrall27@gmail.com>
2024-06-04 15:05:47 +0930

    changes

--- app.py
+++ app.py
@@ -7,4 +7,10 @@ t =(x/y)
 if t != 2:
     print('t is not 2')
 else:
-    print('It is')
+    print('It is 2')
+r=6**2
+f=(r*z)
+if f > t:
+    print("f is greated than t")
+else:
+    print('t is greater than f')
