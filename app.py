956df33b11a8b23659afaf7f442bd79a6b26c425
rileyverrall <rileyverrall27@gmail.com>
2024-06-05 10:26:15 +0930

    fixing

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import math
 from fractions import Fraction
 
 
-def Vector():
+def Magnitude():
     print("Enter values for 3D vector for magnitude")
-    x = float(input('Enter X value: '))
-    y = float(input('Enter Y value: '))
-    z = float(input('Enter Z value: '))
+    x = float(input('X value: '))
+    y = float(input('Y value: '))
+    z = float(input('Z value: '))
     x = x**2
     y = y**2
     z = z**2
@@ -24,36 +24,39 @@ def Vector():
 
 def Angle():
     print("Enter values for 3D vector A ")
-    x = float(input('Enter X value: '))
-    y = float(input('Enter Y value: '))
-    z = float(input('Enter Z value: '))
+    x = float(input('X value: '))
+    y = float(input('Y value: '))
+    z = float(input('Z value: '))
     print("Enter values for 3D vector B ")
-    t = float(input('Enter X value: '))
-    r = float(input('Enter Y value: '))
-    e = float(input('Enter Z value: '))
+    t = float(input('X value: '))
+    r = float(input('Y value: '))
+    e = float(input('Z value: '))
     d = x*t+y*r+z*e
     a = (x**2)+(y**2)+(z**2)
     b = (t**2)+(r**2)+(e**2)
     c = (math.sqrt(a))*(math.sqrt(b))
     g = (math.acos(d/c))
     f = (g*180/math.pi)
-    print(f)
+    h = round(f,1)
+    print("Angle between vectors A and B is:", f, "degrees")
+    print("Angle to 2 decimal places:", h, "degrees")
 
 calcq = input('Chose calculator type: Vector Magnitude "M" or Angle "A" between 2 vectors ')
 if calcq == 'M':
-    Vector()
+    Magnitude()
+elif calcq == 'Magnitude':
+    Magnitude()
+elif calcq == 'magnitude':
+    Magnitude()
 elif calcq == 'm':
-    Vector()
+    Magnitude()
 elif calcq == 'A':
     Angle()
 elif calcq == 'a':
     Angle()
+elif calcq == 'angle':
+    Angle()
+elif calcq == 'Angle':
+    Angle()
 else:
-    print('Not an option.')
-
-
-# p = 1,3,5,3,5,2,6,8,6,8
-# q = 1,1,1,2,2,1,2,1,1,1
-
-# plt.bar(p,q)
-# plt.show()
+    print('Sorry, that was an invalid command!')
